4e67ca57bca8a7f5935ada5a618cbfa191d77bb1
mkayz68 <m.kaizerli@gmail.com>
2020-10-15 12:27:44 +0200

    mes premiers tests

--- src/Participant.php
+++ src/Participant.php
@@ -6,5 +6,50 @@ namespace App;
 
 class Participant
 {
+    private $nom;
+    private $profil;
+
+    /**
+     * @return mixed
+     */
+    public function getProfil()
+    {
+        return $this->profil;
+    }
+
+    /**
+     * @param mixed $profil
+     */
+    public function setProfil($profil)
+    {
+        if($profil === "ASVP" || $profil === "OPEN" || $profil === "Grades")
+        {
+            $this->profil = $profil;
+        }
+        else{
+            throw new \InvalidArgumentException('categorie incorrecte');
+        }
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        if(!preg_match("/^[a-zA-Z]+$/", $nom)){
+
+            throw new \InvalidArgumentException('containe number');
+        }
+        $this->nom = $nom;
+    }
 
 }
---
+++ tests/ParticipantTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Participant;
+
+
+it('test ajout participant', function ()
+{
+        $name = new Participant();
+        $name->setNom("Mohamed");
+        $this->assertSame("Mohamed",$name->getNom());
+
+});
+
+it('test vérifier le nom du participant', function ()
+{
+    $this->expectException(\InvalidArgumentException::class);
+    $name = new Participant();
+    $name->setNom("Mohamed233");
+
+});
+
+it('test ajouter un profil', function ()
+{
+    $prof = new Participant();
+    $prof->setProfil("ASVP");
+    $this->assertSame("ASVP",$prof->getProfil());
+
+});
+
+it('test vérifier le profil du particiapnt', function()
+{
+    $this->expectException(\InvalidArgumentException::class);
+    $prof =  new Participant();
+    $prof->setProfil("GENDARME");
+
+});
