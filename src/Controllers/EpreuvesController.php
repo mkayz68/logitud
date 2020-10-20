4657511ebe92f9d0e9b2bdde61d7d2edf0ecaa49
mkayz68 <m.kaizerli@gmail.com>
2020-10-20 11:23:55 +0200

    nouveau commit

---
+++ src/Controllers/EpreuvesController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class EpreuvesController
+{
+    public function function2()
+    {
+        echo('function2');
+    }
+}
---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{
+
+    public function function1()
+    {
+        echo('function1');
+    }
+}
