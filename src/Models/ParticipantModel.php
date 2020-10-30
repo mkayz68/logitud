fe742567107ea73dac75a3d0fefed79be183a95e
mkayz68 <m.kaizerli@gmail.com>
2020-10-30 17:03:01 +0100

    update bdd

--- src/Models/ParticipantModel.php
+++ src/Models/ParticipantModel.php
@@ -17,38 +17,6 @@ class ParticipantModel extends DataBase {
         $this->pdo = parent::getPdo();
     }
 
-    /*public function addParticipant(Participant $participant)
-    {
-        $req = $this->pdo->prepare("INSERT INTO particpant(nom, prenom, dateDeNaiss, mail, photo, profil_id, categorie_id) VALUE (?,?,?,?,?,?,?)");
-        return $req->execute(array(
-
-            $participant->getNom(),
-            $participant->getPrenom(),
-            $participant->getDateDeNaiss(),
-            $participant->getMail(),
-            $participant->getPhoto(),
-            $participant->getProfilId(),
-            $participant->getCategorieId(),
-        ));
-    }*/
-
-    /*public function create(Request $request)
-    {
-        // enregister $nom dans la table partie
-        $req = "INSERT INTO participant(nom, prenom, dateDeNaiss, mail, photo, profil_id, categorie_id) VALUE(?,?,?,?,?,?,?)";
-        $st = $this->pdo->prepare($req);
-        $st->execute([$request]);
-
-    }*/
-
-    /*public function create($nom)
-    {
-        // enregister $nom dans la table partie
-        $req = "INSERT INTO parti(nom) VALUE(?)";
-        $st = $this->pdo->prepare($req);
-        $st->execute([$nom]);
-    }*/
-
 
     public function add(Request $request)
     {
@@ -56,6 +24,5 @@ class ParticipantModel extends DataBase {
                 VALUES (:nom, :prenom, :dateDeNaiss, :mail, :photo, :profil_id , :categorie_id)";
         $st = $this->pdo->prepare($req);
         $st->execute([$request]);
-
     }
 }
